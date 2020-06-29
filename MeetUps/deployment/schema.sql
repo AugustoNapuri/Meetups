82865bb3466368439ffb6244751a7ef324a4b11d
Augusto_napuri <augustonapuri@gmail.com>
2020-06-29 17:36:16 -0300

    Development enviroment with Docker compose

--- MeetUps/deployment/schema.sql
+++ MeetUps/deployment/schema.sql
@@ -15,27 +15,27 @@ DROP TABLE IF EXISTS Usuario;
 CREATE TABLE Usuario (
     id SERIAL PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
-    tipo_usuario VARCHAR(30) NOT NULL
+    tipoUsuario VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE Meetup (
     id SERIAL PRIMARY KEY,
-    fecha_inicio TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    fecha_fin TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+    fechaInicio TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    fechaFin TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 CREATE TABLE Notificacion (
     id SERIAL PRIMARY KEY,
-    usuario_id INTEGER NOT NULL,
-    meetup_id INTEGER NOT NULL,
+    usuarioId INTEGER NOT NULL,
+    meetupId INTEGER NOT NULL,
     mensaje VARCHAR(300) NOT NULL,
-    fecha_leido TIMESTAMP DEFAULT NULL
+    fechaLeido TIMESTAMP DEFAULT NULL
 );
 
 CREATE TABLE UsuarioMeetup (
-    usuario_id INTEGER NOT NULL,
-    meetup_id INTEGER NOT NULL,
-    fecha_check_in TIMESTAMP DEFAULT NULL,
-    PRIMARY KEY (usuario_id, meetup_id)
+    usuarioId INTEGER NOT NULL,
+    meetupId INTEGER NOT NULL,
+    fechaCheckIn TIMESTAMP DEFAULT NULL,
+    PRIMARY KEY (usuarioId, meetupId)
 );
 
